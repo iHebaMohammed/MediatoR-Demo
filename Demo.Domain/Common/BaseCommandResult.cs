a6bd3f9363df34ac05156e68c11d8b0694746e15
Heba Adel Ahmed <HebaAdelAhmedMohammed@gmail.com>
2025-01-15 10:31:26 +0200

    initaial commit

---
+++ Demo.Domain/Common/BaseCommandResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Domain.Common
+{
+    internal class BaseCommandResult
+    {
+        public bool IsSuccess { get; set; }
+        public StatusCode StatusCode { get; set; }
+        public List<string> Errors { get; set; } = new List<string>();
+    }
+}
---
+++ Demo.Infrastructure/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Infrastructure.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // Entities
+
+    }
+}
